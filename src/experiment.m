ef067aeed6aae556a9d0765ab79f41e4da05083b
strncmp <strnik86@protonmail.com>
2024-05-24 03:07:02 +0300

    Ex 1: Add comments to implementation of experiment

--- src/experiment.m
+++ src/experiment.m
@@ -1,4 +1,9 @@
 function estimation = experiment(data_pdf, estimator, n_samples, n_experiments)
+% EXPERIMENT Runs an estimation experiment on generated samples.
+%   ESTIMATION = EXPERIMENT(pdf, estimator, n_samples, n_experiments)
+%       generates n_samples samples using pdf and calculates an estimation
+%       using estimator. This is repeated n_experiment times. In the end,
+%       the estimation vector contains the estimations of each experiment.
     estimation = zeros(n_experiments, 1);
     for i = 1:n_experiments
         samples = feval(data_pdf, n_samples);
